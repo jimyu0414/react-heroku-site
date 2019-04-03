c785cb81562c2b7247f849516ae2f1f6f3756766
Jim YuL <jimyu0414@gmail.com>
2019-04-03 15:24:11 +1100

    test commit

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (!isDev && cluster.isMaster) {
   // Answer API requests.
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
-    res.send('{"message":"Hello from the custom server!"}');
+    res.send('{"message":"Hello from Commit Test !"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
